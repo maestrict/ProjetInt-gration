8688c56deb0eb74dd9510277ea15d274f6ce5f53
Nlapiere <33150220+Nlapiere@users.noreply.github.com>
2018-10-22 09:05:28 +0200

    Add files via upload

--- table_script.sql
+++ table_script.sql
@@ -18,17 +18,21 @@ CREATE SCHEMA IF NOT EXISTS `integration` DEFAULT CHARACTER SET utf8mb4 COLLATE 
 USE `integration` ;
 
 -- -----------------------------------------------------
--- Table `integration`.`tbUsers`
+-- Table `integration`.`tbClub`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `integration`.`tbUsers` (
-  `userId` INT(11) NOT NULL AUTO_INCREMENT,
-  `userPseudo` VARCHAR(255) NULL DEFAULT NULL,
-  `mdp` VARCHAR(255) NULL DEFAULT NULL,
-  `mail` VARCHAR(255) NULL DEFAULT NULL,
-  `dateInscription` DATE NULL,
-  PRIMARY KEY (`userId`),
-  UNIQUE INDEX `userId_UNIQUE` (`userId` ASC))
+CREATE TABLE IF NOT EXISTS `integration`.`tbClub` (
+  `clubId` INT(11) NOT NULL AUTO_INCREMENT,
+  `clubPseudo` VARCHAR(20) NULL DEFAULT NULL,
+  `Name` VARCHAR(50) NULL DEFAULT NULL,
+  `Address` VARCHAR(50) NULL DEFAULT NULL,
+  `zipCode` INT(11) NULL DEFAULT NULL,
+  `mdp` VARCHAR(50) NULL DEFAULT NULL,
+  `mail` VARCHAR(25) NULL DEFAULT NULL,
+  `DateInscription` DATE NULL DEFAULT NULL,
+  `telephone` VARCHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`clubId`))
 ENGINE = InnoDB
+AUTO_INCREMENT = 2
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -37,33 +41,56 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `integration`.`tbSport`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `integration`.`tbSport` (
-  `sId` INT NOT NULL AUTO_INCREMENT,
+  `sId` INT(11) NOT NULL AUTO_INCREMENT,
   `sport` VARCHAR(255) NOT NULL,
-  `description` LONGTEXT NULL,
+  `description` LONGTEXT NULL DEFAULT NULL,
   PRIMARY KEY (`sId`),
   UNIQUE INDEX `sport_UNIQUE` (`sport` ASC))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
 -- Table `integration`.`tbTerrains`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `integration`.`tbTerrains` (
-  `tId` INT NOT NULL AUTO_INCREMENT,
-  `localite` VARCHAR(255) NOT NULL,
-  `adresse` VARCHAR(255) NOT NULL,
-  `telephone` VARCHAR(255) NOT NULL,
-  `nom` VARCHAR(255) NOT NULL,
-  `sport` VARCHAR(255) NOT NULL,
+  `tId` INT(11) NOT NULL AUTO_INCREMENT,
+  `sId` INT(11) NOT NULL,
+  `clubId` INT(11) NULL DEFAULT NULL,
+  `reserve` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`tId`),
   UNIQUE INDEX `tId_UNIQUE` (`tId` ASC),
-  INDEX `sport_idx` (`sport` ASC),
+  INDEX `sport_idx` (`sId` ASC),
   CONSTRAINT `sport`
-    FOREIGN KEY (`sport`)
-    REFERENCES `integration`.`tbSport` (`sport`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    FOREIGN KEY (`sId`)
+    REFERENCES `integration`.`tbSport` (`sId`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `integration`.`tbUsers`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `integration`.`tbUsers` (
+  `userId` INT(11) NOT NULL AUTO_INCREMENT,
+  `userPseudo` VARCHAR(20) NOT NULL,
+  `LastName` VARCHAR(20) NOT NULL,
+  `FirstName` VARCHAR(20) NOT NULL,
+  `address` VARCHAR(50) NULL DEFAULT NULL,
+  `zipCode` INT(11) NULL DEFAULT NULL,
+  `mdp` VARCHAR(255) NOT NULL,
+  `mail` VARCHAR(25) NOT NULL,
+  `dateInscription` DATE NULL DEFAULT NULL,
+  `dateBirth` DATE NULL DEFAULT NULL,
+  PRIMARY KEY (`userId`),
+  UNIQUE INDEX `userId_UNIQUE` (`userId` ASC),
+  UNIQUE INDEX `userPseudo_UNIQUE` (`userPseudo` ASC))
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;

be2d9e8fb80ac3e64dec8b2faf5c776aa9a26d70
sebiba <sebiba@gmail.com>
2018-11-04 16:54:33 +0100

    Add files via upload
    gestion terrain et sport

--- web/assets/js/utils.js
+++ web/assets/js/utils.js
@@ -1,3 +1,8 @@
+function ajax(rq,idTerrain){
+    //alert(idTerrain);
+    $.post("?rq=assets/php/utils.php",{action: rq, id: idTerrain});
+}
+
 function chg_img(){
     alert("changer votre image avatar?");
 }
